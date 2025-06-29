4619bb4ea14973ca88879c2328ebd1ad26284c8f
c4ps63 <pavlekapuran1@gmail.com>
2025-06-29 13:49:42 +0200

    login/registration design complete

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//Registration pop-up modal control
+
+document.querySelector('#registration').addEventListener('click', ()=>{
+	document.querySelector('.custom-modal').style.display = 'block'
+})
+
+document.querySelector('#closeModal').addEventListener('click', ()=>{
+	document.querySelector('.custom-modal').style.display = 'none'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
